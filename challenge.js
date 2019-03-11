bc5873f297c2c46a8833a4d9671f8fba042f25e4
Matthijs Groen <matthijs@kabisa.nl>
2019-03-11 13:02:31 +0100

    Remove map and reduce

--- challenge.js
+++ challenge.js
@@ -10,17 +10,15 @@ that is a path into this JSON struct",
     "allowed": [
       "Only code in this file",
       "Only functions that take one argument",
-      "Usage of && and ||",
-      ".map(), .reduce()",
-      "Provided head and tail functions",
+      "Ternary operator true ? a : b",
       "Recursion"
     ],
     "disallowed": [
       "No dependencies",
       "No JSON.parse",
-      "Usage of 'if' and ? : (ternary operator)",
-      "Usage for/while loops",
-      "No other host language functions except map and reduce",
+      "Usage of 'if'",
+      "Usage of for/while loops",
+      "No other host language functions except concat",
       "Multiple statements in a lambda"
     ]
   },
@@ -53,17 +51,30 @@ const PARSED = 0;
 const REMAINING = 1;
 const FAILED = Symbol("Failed");
 
+const map = transform => ([head, ...tail]) =>
+  head ? [transform(head), ...map(transform)(tail)] : [];
+
+const doReduce = reducer => start => ([head, ...tail]) =>
+  head ? doReduce(reducer)(reducer(start)(head))(tail) : start;
+
+const reduce = reducer => ([head, ...tail]) => doReduce(reducer)(head)(tail);
+
+console.log(
+  reduce(result => elem => result + elem)(map(x => x + 1)([1, 2, 3]))
+);
+console.log(map(x => x + 1)([1, 2, 3]));
+
 const satisfy = predicate => ([head, ...tail]) =>
-  (predicate(head) && [head, tail]) || [
-    FAILED,
-    "Error parsing:",
-    (head && `Unexpected '${head}'`) || "Unexpected EOF"
-  ];
+  head
+    ? predicate(head)
+      ? [head, tail]
+      : [FAILED, "Error parsing:", `Unexpected '${head}'`]
+    : [FAILED, "Error parsing:", "Unexpected EOF"];
 
 const onSuccess = result => next =>
-  (result[PARSED] !== FAILED && next(result)) || result;
+  result[PARSED] !== FAILED ? next(result) : result;
 const onFailure = result => next =>
-  (result[PARSED] === FAILED && next(result)) || result;
+  result[PARSED] === FAILED ? next(result) : result;
 
 const addLabel = label => parser => stream =>
   onFailure(parser(stream))(([failed, , error]) => [
@@ -96,16 +107,13 @@ const andThen = parserA => parserB => stream =>
 const orElse = parserA => parserB => stream =>
   onFailure(parserA(stream))(() => parserB(stream));
 
-const choice = parsers =>
-  parsers.reduce((result, elem) => orElse(result)(elem));
-
-const chain = parsers =>
-  parsers.reduce((result, elem) => andThen(result)(elem));
+const choice = reduce(orElse);
+const chain = reduce(andThen);
 
 const stringToChars = ([head, ...tail]) =>
-  (head && [head].concat(stringToChars(tail))) || [];
-const anyOf = string => choice(stringToChars(string).map(characterParser));
-const sequence = string => chain(stringToChars(string).map(characterParser));
+  head ? [head].concat(stringToChars(tail)) : [];
+const anyOf = string => choice(map(characterParser)(stringToChars(string)));
+const sequence = string => chain(map(characterParser)(stringToChars(string)));
 const toList = mapResult(result => [result[0]].concat(result[1]));
 const many = parser => stream =>
   orElse(toList(andThen(parser)(many(parser))))(returnResult([]))(stream);
@@ -129,36 +137,55 @@ const sepBy = parser => sepParser =>
   orElse(sepBy1(parser)(sepParser))(returnResult([]));
 
 const toString = mapResult(result =>
-  result[0].concat(result[1]).reduce((str, char) => str + char)
+  reduce(str => char => str + char)(result[0].concat(result[1]))
 );
-const stringParser = string => addLabel(string)(toString(sequence(string)));
+const stringParser = string => addLabel(string)(sequence(string));
 
 const toNumber = mapResult(([sign, digits]) =>
-  Number([sign].concat(digits).reduce((str, char) => str + char))
+  Number(reduce(str => char => str + char)([sign].concat(digits)))
 );
 
-const digits = "01234567890";
-const integerParser = toNumber(
-  andThen(opt(characterParser("-")))(many1(anyOf(digits)))
-);
+const parseStringResult = string => result =>
+  andThenRight(stringParser(string))(returnResult(result));
 
-const whitespace = " \n\t";
-const whitespaceParser = many1(anyOf(whitespace));
-
-const nullParser = andThenRight(stringParser("null"))(returnResult(null));
+const nullParser = parseStringResult("null")(null);
 const boolParser = addLabel("boolean")(
-  orElse(andThenRight(stringParser("true"))(returnResult(true)))(
-    andThenRight(stringParser("false"))(returnResult(false))
-  )
+  orElse(parseStringResult("true")(true))(parseStringResult("false")(false))
 );
 
 const stringCharParser = satisfy(c => c !== '"' && c !== "\\");
+const specialCharacters = [
+  ['\\"', '"'],
+  ["\\\\", "\\"],
+  ["\\/", "/"],
+  ["\\b", "\b"],
+  ["\\f", "\f"],
+  ["\\n", "\n"],
+  ["\\r", "\r"],
+  ["\\t", "\t"]
+];
+const escapedCharParser = choice(
+  map(([match, result]) => parseStringResult(match)(result))(specialCharacters)
+);
 
-console.log(stringCharParser("b"));
-console.log(stringCharParser("\\"));
+//console.log(escapedCharParser("\\\\"));
+console.log(escapedCharParser("\\n"));
+console.log(escapedCharParser("g"));
 
 console.log(nullParser("null"));
 console.log(nullParser("nul"));
 console.log(boolParser("true"));
 console.log(boolParser("tue"));
 console.log(boolParser("false"));
+
+const digits = "01234567890";
+const integerParser = toNumber(
+  andThen(opt(characterParser("-")))(many1(anyOf(digits)))
+);
+console.log(integerParser("12312321"));
+
+/*
+
+const whitespace = " \n\t";
+const whitespaceParser = many1(anyOf(whitespace));
+*/
